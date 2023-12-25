868a9f179a8a27c80cc0842150f19834845e6cf9
Sopanat Khunthapong <153824679+Sopanat2543@users.noreply.github.com>
2023-12-25 20:09:03 +0700

    Update Exercise5_2_Sopanat_K.py

--- Exercise5_2_Sopanat_K.py
+++ Exercise5_2_Sopanat_K.py
@@ -1,1 +1,8 @@
-
+s = int(input("ระยะทาง (หน่วยเป็นกิโลเมตร)"))
+t = int(input("เวลา (หน่วยเป็นชั่วโมง)"))
+v = s/t
+print(v)
+if s and t > 1:
+  print(v,"Km/hr")
+elif s and t < 1:
+  print("Error")
