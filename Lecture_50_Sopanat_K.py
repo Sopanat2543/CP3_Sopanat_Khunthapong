dd71d369a0da0226bc87f6a061ba017e9c672c04
Sopanat Khunthapong <153824679+Sopanat2543@users.noreply.github.com>
2024-01-08 16:57:52 +0700

    Update Lecture_50_Sopanat_K.py

--- Lecture_50_Sopanat_K.py
+++ Lecture_50_Sopanat_K.py
@@ -1,1 +1,15 @@
+# การบวก
+def plusnumber(x,y):
+    print(x+y)
 
+# การลบ
+def minusnumber(x,y):
+    print(x-y)
+
+# การคูณ
+def doublenumber(x,y):
+    print(x*y)
+
+# การหาร
+def halfnumber(x,y):
+    print(x/y)
