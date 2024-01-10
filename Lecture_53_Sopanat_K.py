2dbce7366e78e13ce796fcaa5562dcac9af9c357
Sopanat Khunthapong <153824679+Sopanat2543@users.noreply.github.com>
2024-01-10 17:34:11 +0700

    Update Lecture_53_Sopanat_K.py

--- Lecture_53_Sopanat_K.py
+++ Lecture_53_Sopanat_K.py
@@ -1,2 +1,8 @@
-def vatCalculate():
+def vatCalculate(totalPrice):
+   totalPrice = int(input("totalPrice"))
+   result = totalPrice+totalPrice*7/100
+   return result
+   
+   vatCalculate()
+   print(vatCalculate)
     
