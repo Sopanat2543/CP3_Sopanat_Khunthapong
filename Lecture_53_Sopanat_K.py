f9dbcbe16d8f29797d09c9bac8dcc2a2dc7eead2
Sopanat Khunthapong <153824679+Sopanat2543@users.noreply.github.com>
2024-01-10 17:46:22 +0700

    Update Lecture_53_Sopanat_K.py

--- Lecture_53_Sopanat_K.py
+++ Lecture_53_Sopanat_K.py
@@ -1,8 +1,10 @@
 def vatCalculate(totalPrice):
-   totalPrice = int(input("totalPrice"))
-   result = totalPrice+totalPrice*7/100
-   return result
-   
-   vatCalculate()
-   print(vatCalculate)
+    result = totalPrice + totalPrice * 7/100
+    return result
+
+totalPrice = int(input("Enter total price: "))
+
+result = vatCalculate(totalPrice)
+print("Total price with VAT:", result)
+
     
