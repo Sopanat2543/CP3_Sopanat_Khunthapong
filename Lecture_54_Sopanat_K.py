16532ea58d0d7d903e13ce96b1da9dc69bfe279f
Sopanat Khunthapong <153824679+Sopanat2543@users.noreply.github.com>
2024-01-11 14:46:57 +0700

    Create Lecture_54_Sopanat_K.py

---
+++ Lecture_54_Sopanat_K.py
@@ -0,0 +1,32 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return showMenu()
+    else:
+        False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+    menuSelect()
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    if userSelected == 1 :
+        print("Vat :",vatCalculator(int(input("Input Number : "))))
+    elif userSelected == 2 :
+        print("Price Included vat :",priceCalculator())
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+login()
