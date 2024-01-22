06fcc19ff9bff6e37b8b0e90e693c403cbacf7e4
Sopanat Khunthapong <153824679+Sopanat2543@users.noreply.github.com>
2024-01-22 19:15:25 +0700

    Update Lecture_72_Sopanat_K.py

--- Lecture_72_Sopanat_K.py
+++ Lecture_72_Sopanat_K.py
@@ -4,8 +4,8 @@ def showBill():
     print("---- My Food----")
     totalPrice = 0
     for number in range(len(menuList)):
-        print(menuList[number][0], priceList[number][1])
-        totalPrice += int(priceList[number])
+        print(menuList[number][0], menuList[number][1])
+        totalPrice += int(menuList[number][1])
     print("Total :", totalPrice)
 while True:
     menuName = input("Plese Enter Menu :")
