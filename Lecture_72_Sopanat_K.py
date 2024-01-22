a26b283a2618fd8432fd8a58540f79cbf4e4f0bc
Sopanat Khunthapong <153824679+Sopanat2543@users.noreply.github.com>
2024-01-22 19:08:31 +0700

    Update Lecture_72_Sopanat_K.py

--- Lecture_72_Sopanat_K.py
+++ Lecture_72_Sopanat_K.py
@@ -1,1 +1,17 @@
+menuList = []
 
+def showBill():
+    print("---- My Food----")
+    totalPrice = 0
+    for number in range(len(menuList)):
+        print(menuList[number][0], priceList[number][1])
+        totalPrice += int(priceList[number])
+    print("Total :", totalPrice)
+while True:
+    menuName = input("Plese Enter Menu :")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = input("Price :")
+        menuList.append([menuName,menuPrice])
+showBill()  
